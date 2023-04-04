3ffd0f7cd43ed7649e193e9109db064754d913c5
ahmad <aalmadani111@gmail.com>
2023-04-04 23:40:01 +0200

    update the task line 6

--- HackerRank_Task_1.py
+++ HackerRank_Task_1.py
@@ -10,8 +10,8 @@ earned = 0
 
 for i in range(no_of_customers):
     size, price = map(int, input('size and price').split())
-    if (shoe_size_list[size]):
-        
+    if (shoe_size_list[siz]):
+        earned += price
         shoe_size_list[size] -= 1
 
 
